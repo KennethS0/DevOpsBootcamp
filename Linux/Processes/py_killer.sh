d22832d8b0fb6b3fa4313b9201068dedf9651c15
KennethS0 <kenneth.sanchez0609@gmail.com>
2025-08-07 21:40:24 -0600

    Python Process Monitoring

--- Linux/Processes/py_killer.sh
+++ Linux/Processes/py_killer.sh
@@ -2,7 +2,7 @@
 
 while true; do
 	ps aux | grep python | awk '$3>20.0{print "PID: " $2 " CMD: " $11 " is consuming more than 20% the CPU power: " $3}'
-
+	echo "======================================================================"
 	pids=$(ps aux | grep python | awk '$3>50.0{print $2}')
 	for pid in $pids; do
 		echo "Killing process $pid consuming more than 50% CPU utilization..."
